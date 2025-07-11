55e4749ee51937750ba708c5edd3dc1c6e90d83a
Marco Scholl <develop@marco-scholl.de>
2025-07-11 22:52:38 +0200

    Add additional pin definitions for Smart-MF Soundmodul

--- include/HardwareConfig/OpenKNX-Ready/Smart-MF/Devices.h
+++ include/HardwareConfig/OpenKNX-Ready/Smart-MF/Devices.h
@@ -18,6 +18,38 @@
  */
 
 /************************************************** */
+//  UP
+/************************************************** */
+#ifdef DEVICE_SMARTMF_SOM_UP
+    #define PROG_LED_PIN 2
+    #define PROG_LED_PIN_ACTIVE_ON HIGH
+    #define INFO1_LED_PIN 29
+    #define INFO1_LED_PIN_ACTIVE_ON HIGH
+    #define PROG_BUTTON_PIN 0
+    #define FUNC1_BUTTON_PIN 1
+    #define SAVE_INTERRUPT_PIN 13
+    #define KNX_UART_NUM 0
+    #define KNX_UART_RX_PIN 17
+    #define KNX_UART_TX_PIN 16
+    #define PLAYER_UART_RX_PIN 9
+    #define PLAYER_UART_TX_PIN 8
+    #define PLAYER_I2S_BCLK_PIN 8
+    #define PLAYER_I2S_DATA_PIN 20
+    #define PLAYER_POWER_PIN 3
+    #define DEVICE_ID "SOM-UP"
+    #define DEVICE_NAME "Smart-MF Soundmodul"
+
+    #define SmartMF_HardwareRevision_PIN1 10
+    #define SmartMF_HardwareRevision_PIN2 11
+    #define SmartMF_HardwareRevision_PIN3 12
+
+    #define OPENKNX_BI_GPIO_PINS 4, 5, 19, 18
+    #define OPENKNX_BI_GPIO_COUNT 4
+    #define OPENKNX_BI_PULSE 14
+#endif
+
+
+/************************************************** */
 //  REG 1TE
 /************************************************** */
 
