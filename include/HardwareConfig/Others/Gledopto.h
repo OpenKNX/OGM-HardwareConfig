8bf530ae0a3f7e9a2ad8c16f99dd5d4495aae52c
Franz Reisenhofer <franz.reisenhofer@me.com>
2026-06-17 12:59:55 +0200

    add Gledopto GL-C-620WL

--- include/HardwareConfig/Others/Gledopto.h
+++ include/HardwareConfig/Others/Gledopto.h
@@ -84,4 +84,101 @@
 
 #endif
 
+#ifdef DEVICE_GLEDOPTO_GL_C_620WL
+// ============================================================================
+// Gledopto GL-C-620WL - ESP32 LAN Controller
+// ============================================================================
+// Product: GL-C-620WL
+// Chip:    ESP32-D0WD-V3 (Revision 3.1)
+// Network: LAN8720A Ethernet PHY connected to the ESP32 RMII interface
+//
+// Confirmed IO-Port map:
+//   GPIO13 - Programming / function button
+//   GPIO16 - LED data output 1 (Strip1)
+//   GPIO2  - LED data output 2 (Strip2)
+//   GPIO18 - Relay output
+//
+// Confirmed RMII data lines: (fixed ESP32 RMII pin)
+//   GPIO21 - LAN8720A TX_EN
+//   GPIO26 - LAN8720A RXD1
+//   GPIO25 - LAN8720A RXD0
+//   GPIO22 - LAN8720A TXD1
+//   GPIO19 - LAN8720A TXD0
+//   GPIO27 - LAN8720A CRS_DV
+// ============================================================================
+
+    #define GLEDOPTO_BUTTON_GPIO 13
+    #define GLEDOPTO_LED_DATA1_GPIO 16
+    #define GLEDOPTO_LED_DATA2_GPIO 2
+    #define GLEDOPTO_RELAIS1_GPIO 18
+
+    #define GLEDOPTO_ETHERNET_TXEN_GPIO 21
+    #define GLEDOPTO_ETHERNET_TXD0_GPIO 19
+    #define GLEDOPTO_ETHERNET_TXD1_GPIO 22
+    #define GLEDOPTO_ETHERNET_RXD0_GPIO 25
+    #define GLEDOPTO_ETHERNET_RXD1_GPIO 26
+    #define GLEDOPTO_ETHERNET_CRS_DV_GPIO 27
+
+    #define GLEDOPTO_ETHERNET_MDC_GPIO 23
+    #define GLEDOPTO_ETHERNET_MDIO_GPIO 33
+    #define GLEDOPTO_ETHERNET_POWER_GPIO 5
+    #define GLEDOPTO_ETHERNET_CLK_MODE ETH_CLOCK_GPIO0_IN
+
+    #define DEVICE_HW_ID 0x2001
+    #define DEVICE_ID "GL-C-620WL"
+    #define DEVICE_NAME "Gledopto GL-C-620WL ESP32"
+
+    #if !defined(KNX_IP_LAN) && !defined(KNX_IP_WIFI)
+        #define KNX_IP_LAN
+    #endif
+
+    #ifndef ETH_PHY_TYPE
+        #define ETH_PHY_TYPE ETH_PHY_LAN8720
+    #endif
+    #ifndef ETH_PHY_ADDR
+        #define ETH_PHY_ADDR 1
+    #endif
+    #ifndef ETH_PHY_MDC
+        #define ETH_PHY_MDC GLEDOPTO_ETHERNET_MDC_GPIO
+    #endif
+    #ifndef ETH_PHY_MDIO
+        #define ETH_PHY_MDIO GLEDOPTO_ETHERNET_MDIO_GPIO
+    #endif
+    #ifndef ETH_PHY_POWER
+        #define ETH_PHY_POWER GLEDOPTO_ETHERNET_POWER_GPIO
+    #endif
+    #ifndef ETH_CLK_MODE
+        #define ETH_CLK_MODE GLEDOPTO_ETHERNET_CLK_MODE
+    #endif
+
+    #ifndef SERIAL_DEBUG
+        #define SERIAL_DEBUG Serial
+    #endif
+
+    #ifndef KNX_SERIAL
+        #define KNX_SERIAL Serial1
+    #endif
+    #ifndef KNX_UART_NUM
+        #define KNX_UART_NUM 1
+    #endif
+    #ifndef KNX_UART_RX_PIN
+        #define KNX_UART_RX_PIN 36
+    #endif
+    #ifndef KNX_UART_TX_PIN
+        #define KNX_UART_TX_PIN 3
+    #endif
+
+    #ifndef PROG_LED_PIN
+        #define PROG_LED_PIN (-1) // No dedicated prog LED for this hardware
+        #ifndef PROG_LED_PIN_ACTIVE_ON
+            #define PROG_LED_PIN_ACTIVE_ON HIGH
+        #endif
+    #endif
+
+    #ifndef PROG_BUTTON_PIN
+        #define PROG_BUTTON_PIN GLEDOPTO_BUTTON_GPIO
+    #endif
+
+#endif
+
 #pragma endregion
